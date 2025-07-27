c277eda3ef9e75e61474223f54a4333a8c68228c
Maximiliano-Dutra <maximiliano@bemcuidardesenvolvimento.com.br>
2025-07-27 19:00:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function showPage(pageId) {
+  document.querySelectorAll('.page').forEach(page => {
+    page.style.display = 'none';
+  });
+  document.getElementById(pageId).style.display = 'block';
+}
